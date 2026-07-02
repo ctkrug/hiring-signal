dc0c147d26b3b31ab67678ea6088c2c4d48816be
ctkrug <ctkrug4501@gmail.com>
2026-07-02 18:06:32 +0000

    chore: add eslint configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  env: { es2022: true, node: true, browser: true },
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  ignorePatterns: ["dist/", "node_modules/"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+  },
+};
